6f34814f8f3b97decac03c60dcbe6cef0cc3895b
Joel Martinez <joematz@live.com>
2024-01-18 21:36:32 -0600

    Adding new  squema to post

--- src/models/posts.js
+++ src/models/posts.js
@@ -1,32 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-//Esquema 
+//Esquema
 const postSchema = new mongoose.Schema({
-    date: { 
-        type: String
-    },
-    dateMiliseconds: {  
-        type: Number
-    },
-    reactions: { 
-        type: Number
-    },
-    description: {
-        required: true,  
-        type: String
-    },
-    title: {
-        required: true,  
-        type: String
-    },
-    url: {
-        required: true,  
-        type: String
-    },
-    tags: {  
-        type: Array
-    }, 
+  date: {
+    type: String,
+  },
+  dateMiliseconds: {
+    type: Number,
+  },
+  reactions: {
+    type: Number,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  title: {
+    required: true,
+    type: String,
+  },
+  url: {
+    required: true,
+    type: String,
+  },
+  tags: {
+    type: Array,
+  },
+  picture: {
+    type: String,
+  },
+  author: {
+    type: String,
+  },
 });
 
-const Posts =  mongoose.model("Posts", postSchema) // Modelo 
-module.exports = Posts
+const Posts = mongoose.model("Posts", postSchema); // Modelo
+module.exports = Posts;
